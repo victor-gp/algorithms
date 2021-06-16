4bf78cae8f31e83d10a56d5ff3093f58f6edadc1
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-06-16 18:22:51 +0200

    Add the starter code from CodinGame

--- codingame/ocean-of-code/src/main.rs
+++ codingame/ocean-of-code/src/main.rs
@@ -1,3 +1,54 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
 fn main() {
-    println!("Hello, world!");
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    let width = parse_input!(inputs[0], i32);
+    let height = parse_input!(inputs[1], i32);
+    let my_id = parse_input!(inputs[2], i32);
+    for i in 0..height as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let line = input_line.trim_matches('\n').to_string();
+    }
+
+    // Write an action using println!("message...");
+    // To debug: eprintln!("Debug message...");
+
+    println!("7 7");
+
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let x = parse_input!(inputs[0], i32);
+        let y = parse_input!(inputs[1], i32);
+        let my_life = parse_input!(inputs[2], i32);
+        let opp_life = parse_input!(inputs[3], i32);
+        let torpedo_cooldown = parse_input!(inputs[4], i32);
+        let sonar_cooldown = parse_input!(inputs[5], i32);
+        let silence_cooldown = parse_input!(inputs[6], i32);
+        let mine_cooldown = parse_input!(inputs[7], i32);
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let sonar_result = input_line.trim().to_string();
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let opponent_orders = input_line.trim_matches('\n').to_string();
+
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+
+        println!("MOVE N TORPEDO");
+    }
 }
