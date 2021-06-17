9cdd4eda83ab454d3abb07f39a4027f8a752ec7a
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-06-17 12:53:09 +0200

    ocean-of-code: properly read the game map

--- codingame/ocean-of-code/src/main.rs
+++ codingame/ocean-of-code/src/main.rs
@@ -4,10 +4,8 @@ macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+const GAME_TURNS: u16 = 300;
+
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
@@ -15,19 +13,18 @@ fn main() {
     let width = parse_input!(inputs[0], i32);
     let height = parse_input!(inputs[1], i32);
     let my_id = parse_input!(inputs[2], i32);
-    for i in 0..height as usize {
-        let mut input_line = String::new();
-        io::stdin().read_line(&mut input_line).unwrap();
-        let line = input_line.trim_matches('\n').to_string();
-    }
+    let map = Map::read(width, height);
+    // eprintln!("{}", map.to_string());
 
-    // Write an action using println!("message...");
-    // To debug: eprintln!("Debug message...");
+    let mut turn = 1u16;
 
+    // starting position, turn 1
     println!("7 7");
 
     // game loop
     loop {
+        turn += 1;
+
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let inputs = input_line.split(" ").collect::<Vec<_>>();
@@ -46,9 +43,66 @@ fn main() {
         io::stdin().read_line(&mut input_line).unwrap();
         let opponent_orders = input_line.trim_matches('\n').to_string();
 
-        // Write an action using println!("message...");
-        // To debug: eprintln!("Debug message...");
-
         println!("MOVE N TORPEDO");
+    }
+}
+
+struct Map {
+    width: i32,
+    height: i32,
+    grid: Vec<Vec<Cell>>,
+}
+
+impl Map {
+    fn read(width: i32, height: i32) -> Map {
+        let mut grid = Vec::new();
+
+        for i in 0..height as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let line = input_line.trim_matches('\n').to_string();
+            // eprintln!("{}",line);
+
+            grid.push(
+                line.chars()
+                .map(|c| Cell::from_char(c))
+                .collect::<Vec<Cell>>()
+            )
+        }
+
+        Map { width, height, grid }
+    }
+
+    fn to_string(&self) -> String {
+        let preamble = format!("<Map>\nwidth: {} / height: {}\n", self.width, self.height);
+        let grid = self.grid.iter()
+            .map(|row| row.iter()
+                .map(|cell| cell.to_char())
+                .collect::<String>()
+            ).collect::<Vec<String>>()
+            .join("\n");
+
+        preamble + &grid + "\n</Map>"
+    }
+}
+
+enum Cell {
+    Water, Land
+}
+
+impl Cell {
+    fn from_char(c: char) -> Cell {
+        match c {
+            '.' => Cell::Water,
+            'x' => Cell::Land,
+            _ => panic!("invalid cell")
+        }
+    }
+
+    fn to_char(&self) -> char {
+        match self {
+            Cell::Water => '.',
+            Cell::Land  => 'x'
+        }
     }
 }
