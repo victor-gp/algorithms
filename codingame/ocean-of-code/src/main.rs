816203bf57e924c31f51b89fc7673e03a593e36b
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-06-18 10:11:38 +0200

    ocean-of-code: misc
    - remove debug prints that are no longer needed
    - derive PartialEq rather than implementing it
    - derive Eq cause why not
    - turn i32 into usize wherever it makes sense

--- codingame/ocean-of-code/src/main.rs
+++ codingame/ocean-of-code/src/main.rs
@@ -27,13 +27,13 @@ fn main() {
 
 struct Global {
     map: Map,
-    turn: i32,
-    game_turns: i32,
+    turn: usize,
+    game_turns: usize,
     me_first: bool,
 }
 
 struct Me {
-    lives: i32,
+    lives: usize,
     pos: Coord,
     visited: Vec<Coord>,
     torpedo_cooldown: i32,
@@ -43,7 +43,7 @@ struct Me {
 }
 
 struct Opponent {
-    lives: i32,
+    lives: usize,
     // likely_pos: Iter<'static, Coord>,
     // visited: Vec<Coord>,
     // move_history: Vec<Move>,
@@ -51,13 +51,13 @@ struct Opponent {
 }
 
 struct Map {
-    width: i32,
-    height: i32,
+    width: usize,
+    height: usize,
     grid: Vec<Vec<Cell>>,
     water: Vec<Coord>,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 struct Coord {
     x: usize,
     y: usize
@@ -69,12 +69,12 @@ fn read_starting_info() -> Global {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let inputs = input_line.split(" ").collect::<Vec<_>>();
-    let width = parse_input!(inputs[0], i32);
-    let height = parse_input!(inputs[1], i32);
-    let my_id = parse_input!(inputs[2], i32);
+    let width = parse_input!(inputs[0], usize);
+    let height = parse_input!(inputs[1], usize);
+    let my_id = parse_input!(inputs[2], usize);
 
     let mut grid = Vec::new();
-    for i in 0..height as usize {
+    for i in 0..height {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let line = input_line.trim_matches('\n').to_string();
@@ -104,8 +104,8 @@ fn read_turn_info(global: &Global, me: &mut Me, opp: &mut Opponent) {
     let inputs = input_line.split(" ").collect::<Vec<_>>();
     let x = parse_input!(inputs[0], usize);
     let y = parse_input!(inputs[1], usize);
-    let my_life = parse_input!(inputs[2], i32);
-    let opp_life = parse_input!(inputs[3], i32);
+    let my_life = parse_input!(inputs[2], usize);
+    let opp_life = parse_input!(inputs[3], usize);
     let torpedo_cooldown = parse_input!(inputs[4], i32);
     let sonar_cooldown = parse_input!(inputs[5], i32);
     let silence_cooldown = parse_input!(inputs[6], i32);
@@ -183,7 +183,7 @@ impl Cell {
 use std::slice::Iter;
 
 impl Map {
-    fn new(width: i32, height: i32, grid: Vec<Vec<Cell>>) -> Map {
+    fn new(width: usize, height: usize, grid: Vec<Vec<Cell>>) -> Map {
         let water = Map::water_positions(&grid);
 
         Map { width, height, grid, water }
@@ -240,8 +240,6 @@ impl Action {
         let adjacents = pos.adjacents_clockwise();
         let mut valid_moves = Vec::new();
 
-        eprintln!("{:?}", adjacents);
-
         for i in 0..=3 {
             let pos_i = adjacents[i];
             if map.is_water(pos_i) && !visited.contains(&pos_i) {
@@ -250,8 +248,6 @@ impl Action {
                 );
             }
         }
-
-        eprintln!("{:?}", valid_moves);
 
         valid_moves
     }
@@ -284,12 +280,6 @@ impl Coord {
     }
 }
 
-impl PartialEq for Coord {
-    fn eq(&self, other: &Self) -> bool {
-        self.x == other.x && self.y == other.y
-    }
-}
-
 impl Map {
     fn is_water(&self, coord: Coord) -> bool {
         match self.cell_at(coord) {
@@ -299,8 +289,8 @@ impl Map {
     }
 
     fn is_within_bounds(&self, coord: Coord) -> bool {
-        coord.x < self.width as usize
-            && coord.y < self.height as usize
+        coord.x < self.width
+            && coord.y < self.height
     }
 
     fn cell_at(&self, coord: Coord) -> Option<&Cell> {
