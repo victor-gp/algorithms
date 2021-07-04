be0cad7e491a80207cb4996424555e08911544ab
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-04 12:10:04 +0200

    Add starter code for Kakuro Solver puzzle

---
+++ codingame/puzzles/kakuro-solver.rb
@@ -0,0 +1,14 @@
+# https://www.codingame.com/training/hard/kakuro-solver
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+height, width = gets.split(" ").collect { |x| x.to_i }
+height.times do
+    line = gets.chomp
+end
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+puts "Kakuro grid"
