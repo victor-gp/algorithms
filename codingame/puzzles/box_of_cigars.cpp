fc656cc523d7111eeb93d70f503a6e552bb63c0b
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-19 20:50:00 +0200

    Add timeout-ing solution, with improvements
    Still failing but this one has mechanisms for early return in the
    recursion:
    
    - when the delta between the previous selected cigar and the current is
    larger than the locked delta for the sequence (duh)
    - when the length of the sequence so far plus the amount of cigars left
    is less than the max sequence found so far.
    
    Still timing out though. So there's probably a smarter algorithm for
    solving this...

--- codingame/puzzles/box_of_cigars.cpp
+++ codingame/puzzles/box_of_cigars.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int largest_poirot_seq(vector<int>, int, int, int);
+int largest_poirot_seq(vector<int>, int, int, int, int, int);
 
 int main() {
     int N;
@@ -14,30 +14,37 @@ int main() {
         cin >> lengths[i]; // already sorted asc
 
     cout
-        << largest_poirot_seq(lengths, -1, 0, -1)
+        << largest_poirot_seq(lengths, -1, 0, -1, 0, 0)
         << endl;
 }
 
 int largest_poirot_seq(
-    vector<int> lengths, int pre_i, int i, int delta
+    vector<int> lengths, int pre_i, int i,
+    int seq_delta, int seq_length, int current_max
 ) {
-    if (i == lengths.size()) return 0;
+    if (i == lengths.size()) return seq_length;
+    int cigars_left = lengths.size() - i;
+    if (seq_length + cigars_left <= current_max) return 0;
 
-    if (pre_i == -1) {
-        int max_with = 1 + largest_poirot_seq(lengths, i, i+1, -1);
-        int max_without = largest_poirot_seq(lengths, -1, i+1, -1);
+    if (seq_length == 0) {
+        int max_with = largest_poirot_seq(lengths, i, i+1, -1, 1, current_max);
+        current_max = max(current_max, max_with);
+        int max_without = largest_poirot_seq(lengths, -1, i+1, -1, 0, current_max);
         return max(max_with, max_without);
     }
-    else if (delta == -1) {
+    else if (seq_length == 1) {
         int delta = lengths[i] - lengths[pre_i];
-        int max_with = 1 + largest_poirot_seq(lengths, i, i+1, delta);
-        int max_without = largest_poirot_seq(lengths, pre_i, i+1, -1);
+        int max_with = largest_poirot_seq(lengths, i, i+1, delta, 2, current_max);
+        current_max = max(current_max, max_with);
+        int max_without = largest_poirot_seq(lengths, pre_i, i+1, -1, 1, current_max);
         return max(max_with, max_without);
     }
 
-    bool matches = delta == lengths[i] - lengths[pre_i];
-    if (matches)
-        return 1 + largest_poirot_seq(lengths, i, i+1, delta);
+    int this_delta = lengths[i] - lengths[pre_i];
+    if (this_delta > seq_delta)
+        return seq_length;
+    else if (this_delta == seq_delta)
+        return largest_poirot_seq(lengths, i, i+1, seq_delta, seq_length+1, current_max);
     else
-        return largest_poirot_seq(lengths, pre_i, i+1, delta);
+        return largest_poirot_seq(lengths, pre_i, i+1, seq_delta, seq_length, current_max);
 }
