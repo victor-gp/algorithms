ab0bdf1c92fb105800c4326ef611933ed00a24a3
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-19 20:04:19 +0200

    Add timeout-ing solution to Box of Cigars
    Passes all pretests (up to 100 cigars) except the last one (1000
    cigars), where it times out.
    
    Gotta think a more efficient algorithm...

--- codingame/puzzles/box_of_cigars.cpp
+++ codingame/puzzles/box_of_cigars.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+int largest_poirot_seq(vector<int>, int, int, int);
 
-int main()
-{
+int main() {
     int N;
-    cin >> N; cin.ignore();
-    for (int i = 0; i < N; i++) {
-        int LNT;
-        cin >> LNT; cin.ignore();
+    cin >> N;
+
+    vector<int> lengths(N);
+    for (int i = 0; i < N; i++)
+        cin >> lengths[i]; // already sorted asc
+
+    cout
+        << largest_poirot_seq(lengths, -1, 0, -1)
+        << endl;
+}
+
+int largest_poirot_seq(
+    vector<int> lengths, int pre_i, int i, int delta
+) {
+    if (i == lengths.size()) return 0;
+
+    if (pre_i == -1) {
+        int max_with = 1 + largest_poirot_seq(lengths, i, i+1, -1);
+        int max_without = largest_poirot_seq(lengths, -1, i+1, -1);
+        return max(max_with, max_without);
+    }
+    else if (delta == -1) {
+        int delta = lengths[i] - lengths[pre_i];
+        int max_with = 1 + largest_poirot_seq(lengths, i, i+1, delta);
+        int max_without = largest_poirot_seq(lengths, pre_i, i+1, -1);
+        return max(max_with, max_without);
     }
 
-    // Write an answer using cout. DON'T FORGET THE "<< endl"
-    // To debug: cerr << "Debug messages..." << endl;
-
-    cout << "2" << endl;
+    bool matches = delta == lengths[i] - lengths[pre_i];
+    if (matches)
+        return 1 + largest_poirot_seq(lengths, i, i+1, delta);
+    else
+        return largest_poirot_seq(lengths, pre_i, i+1, delta);
 }
