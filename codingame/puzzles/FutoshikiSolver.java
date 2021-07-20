b82ce7e7fd255a77d66863dcf5ef73d48a6871af
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-20 18:32:56 +0200

    Add starter Java code for Futoshiki Solver

---
+++ codingame/puzzles/FutoshikiSolver.java
@@ -0,0 +1,28 @@
+// https://www.codingame.com/ide/puzzle/futoshiki-solver
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        for (int i = 0; i < size; i++) {
+            String line = in.nextLine();
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
