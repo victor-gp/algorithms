4ba35c85b2d62ff78b4856dea6863478b8de8d89
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-20 19:40:17 +0200

    Model the Futoshiki domain

--- codingame/puzzles/FutoshikiSolver.java
+++ codingame/puzzles/FutoshikiSolver.java
@@ -1,13 +1,10 @@
 // https://www.codingame.com/ide/puzzle/futoshiki-solver
+// https://en.wikipedia.org/wiki/Futoshiki
 
 import java.util.*;
 import java.io.*;
 import java.math.*;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Solution {
 
     public static void main(String args[]) {
@@ -19,10 +16,75 @@ class Solution {
         for (int i = 0; i < size; i++) {
             String line = in.nextLine();
         }
+        in.close();
 
-        // Write an answer using System.out.println()
-        // To debug: System.err.println("Debug messages...");
+        Futoshiki puzzle = new Futoshiki();
+        puzzle.solve();
 
-        System.out.println("answer");
+        System.out.println(puzzle);
+    }
+}
+
+class Futoshiki {
+    int size;
+    Vector<Vector<Cell>> grid;
+
+    public Futoshiki() {}
+
+    public void solve() {}
+    // dumb backtracking
+
+    public String toString() {
+        return "stub";
+    }
+}
+
+class Cell {
+    public int value;
+    private Constraint right;
+    private Constraint down;
+
+    public Cell(int value, char right, char down) {
+        this.value = value;
+        this.right = Constraint.fromChar(right);
+        this.down = Constraint.fromChar(down);
+    }
+
+    public boolean validateRight(int rightValue) {
+        return right.validate(value, rightValue);
+    }
+
+    public boolean validateDown(int downValue) {
+        return down.validate(value, downValue);
+    }
+}
+
+abstract class Constraint {
+
+    public static Constraint fromChar(char constraintChar) {
+        return new None(); //todo
+    }
+
+    public abstract boolean validate(int value, int nextValue);
+}
+
+class None extends Constraint {
+
+    public boolean validate(int value, int nextValue) {
+        return true;
+    }
+}
+
+class More extends Constraint {
+
+    public boolean validate(int value, int nextValue) {
+        return value > nextValue;
+    }
+}
+
+class Less extends Constraint {
+
+    public boolean validate(int value, int nextValue) {
+        return value < nextValue;
     }
 }
