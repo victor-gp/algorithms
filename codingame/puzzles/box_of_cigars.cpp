83b93545ca44933cb69fa661419ce9f3a7170c3e
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-20 16:25:36 +0200

    Add final solution for Box of Cigars
    So the solution needed to be DP. I was doing brute-force combinatorics
    before, and I was repeating a lot of computations in that recursion.
    
    A simple O(n log n) traversal filling an O(n*m) matrix, m being the size
    of the largest cigar, does the trick.

--- codingame/puzzles/box_of_cigars.cpp
+++ codingame/puzzles/box_of_cigars.cpp
@@ -3,48 +3,27 @@
 
 using namespace std;
 
-int largest_poirot_seq(vector<int>, int, int, int, int, int);
-
 int main() {
     int N;
     cin >> N;
 
-    vector<int> lengths(N);
+    vector<int> cigar_lengths(N);
     for (int i = 0; i < N; i++)
-        cin >> lengths[i]; // already sorted asc
+        cin >> cigar_lengths[i]; // already sorted asc
 
-    cout
-        << largest_poirot_seq(lengths, -1, 0, -1, 0, 0)
-        << endl;
-}
+    // possible deltas are [0, cigar_max_length)
+    int delta_upper_bound = cigar_lengths.back();
+    // length of the sequence for every possible delta, at every cigar
+    vector<vector<int>> seq_lengths(N, vector<int>(delta_upper_bound, 1));
 
-int largest_poirot_seq(
-    vector<int> lengths, int pre_i, int i,
-    int seq_delta, int seq_length, int current_max
-) {
-    if (i == lengths.size()) return seq_length;
-    int cigars_left = lengths.size() - i;
-    if (seq_length + cigars_left <= current_max) return 0;
+    int poirot_number = 0;
+    for (int i = 1; i < N; i++)
+        for (int j = 0; j < i; j++) {
+            int delta = cigar_lengths[i] - cigar_lengths[j];
+            seq_lengths[i][delta] = seq_lengths[j][delta] + 1;
+            if (seq_lengths[i][delta] > poirot_number)
+                poirot_number = seq_lengths[i][delta];
+        }
 
-    if (seq_length == 0) {
-        int max_with = largest_poirot_seq(lengths, i, i+1, -1, 1, current_max);
-        current_max = max(current_max, max_with);
-        int max_without = largest_poirot_seq(lengths, -1, i+1, -1, 0, current_max);
-        return max(max_with, max_without);
-    }
-    else if (seq_length == 1) {
-        int delta = lengths[i] - lengths[pre_i];
-        int max_with = largest_poirot_seq(lengths, i, i+1, delta, 2, current_max);
-        current_max = max(current_max, max_with);
-        int max_without = largest_poirot_seq(lengths, pre_i, i+1, -1, 1, current_max);
-        return max(max_with, max_without);
-    }
-
-    int this_delta = lengths[i] - lengths[pre_i];
-    if (this_delta > seq_delta)
-        return seq_length;
-    else if (this_delta == seq_delta)
-        return largest_poirot_seq(lengths, i, i+1, seq_delta, seq_length+1, current_max);
-    else
-        return largest_poirot_seq(lengths, pre_i, i+1, seq_delta, seq_length, current_max);
+    cout << poirot_number << endl;
 }
