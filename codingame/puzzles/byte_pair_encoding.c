14912f7ebb32f17cba2fc3250151bbab8e144b6c
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-21 17:26:58 +0200

    Add structs for Byte Pair Encoding

--- codingame/puzzles/byte_pair_encoding.c
+++ codingame/puzzles/byte_pair_encoding.c
@@ -5,20 +5,43 @@
 #include <string.h>
 #include <stdbool.h>
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+typedef char byte_pair[2];
 
-int main()
-{
-    int n;
-    int m;
+struct production_rule {
+    char symbol;
+    byte_pair sub;
+};
+
+struct pair_freq {
+    byte_pair pair;
+    uint freq;
+};
+
+struct encoding {
+    char (*text)[];
+    struct production_rule (*rules)[];
+};
+
+struct encoding encode(char text[], struct production_rule rules[], char current_non_terminal);
+
+int main() {
+    int n; // n lines
+    int m; // of length m
     scanf("%d%d", &n, &m);
-    for (int i = 0; i < n; i++) {
-        char line[m + 1];
-        scanf("%s", line);
-    }
+
+    char text[n*m];
+    for (int i = 0; i < n; i++)
+        scanf("%s", &text[i*m]);
+
+    struct production_rule rules[0];
+    struct encoding enc = encode(text, rules, 'Z');
+
+    printf("%s", text);
 
     return 0;
+}
+
+struct encoding encode(char text[], struct production_rule rules[], char current_non_terminal) {
+    struct encoding enc;
+    return enc;
 }
