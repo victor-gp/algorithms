08789eaec0e5888246bd355b88aa95b5e77fabde
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-21 15:22:13 +0200

    Add starter C code for Byte Pair Encoding

---
+++ codingame/puzzles/byte_pair_encoding.c
@@ -0,0 +1,24 @@
+// https://www.codingame.com/ide/puzzle/byte-pair-encoding
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int n;
+    int m;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        char line[m + 1];
+        scanf("%s", line);
+    }
+
+    return 0;
+}
